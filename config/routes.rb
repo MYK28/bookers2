4381543455366ca4a4871358d10a1eb067680a3a
MYK28 <myams1128@gmail.com>
2021-05-21 08:50:31 +0000

    [Add]Book-index

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
 
-  resources :users, only: [:index, :show]
+  resources :users, only:[:index, :show, :edit, :update]
+  resources :books, only:[:index, :create, :show, :edit, :destroy]
 end
