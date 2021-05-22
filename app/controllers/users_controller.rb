e346d4e0d5c11aadcdab77a56a9eaccf7b9ec60e
MYK28 <myams1128@gmail.com>
2021-05-22 04:51:16 +0000

    [Add]User-index-userinfo,newbook

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,20 @@
 class UsersController < ApplicationController
   def index
+    @user = current_user
+    @book = Book.new
     @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
   end
 
   def edit
@@ -11,10 +25,6 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(@user.id)
-  end
-
-  def show
-    @user = User.find(params[:id])
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
 
-  resources :users, only:[:index, :show, :edit, :update]
+  resources :users, only:[:index, :create, :show, :edit, :update]
   resources :books, only:[:index, :create, :show, :edit, :destroy]
 end
